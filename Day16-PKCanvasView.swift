fd1c1d5a5273fe28a0d5599ff448a15ae37a970e
Dali Han <ross4han@gmail.com>
2020-08-08 18:20:11 +0800

    update D16 PKCanvas with Menu

--- Day16-PKCanvasView.swift
+++ Day16-PKCanvasView.swift
@@ -5,7 +5,7 @@
 //  Created by Dali Han on 2020/8/4.
 //  Copyright © 2020 Dali Han. All rights reserved.
 //  Credit to Kavsoft
-//. Link: https://www.youtube.com/watch?v=LR-ttBoa89M
+//  Link: https://www.youtube.com/watch?v=LR-ttBoa89M
 
 import SwiftUI
 import PencilKit
@@ -22,109 +22,117 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Home: View {
-    
+struct  Home: View {
     @State var canvas = PKCanvasView()
     @State var isDraw = true
-    @State var color : Color = .black
+    @State var color: Color = .black
     @State var type: PKInkingTool.InkType = .pencil
+    @State var colorPicker = false
     
     var body: some View {
         NavigationView {
-            // Drawing View
             DrawingView(canvas: $canvas, isDraw: $isDraw, type: $type, color: $color)
                 .navigationTitle("Drawing")
                 .navigationBarTitleDisplayMode(.inline)
                 .navigationBarItems(leading: Button(action: {
-                    // saving image
-                    SaveImage()
-                }, label: {
-                    
-                    Image(systemName: "pencil.tip.crop.circle.badge.plus")
-                        .font(.headline)
-                }), trailing: HStack(spacing: 15) {
-                    
+                    // saving images
+                }) {
+                    Image(systemName: "square.and.arrow.down.fill")
+                        .font(.title)
+                }, trailing: HStack(spacing: 15) {
                     Button(action: {
-                        
-                        isDraw.toggle()
-                        
-                    }, label: {
+                        // eraser tool
+                        isDraw = false
+                    }) {
                         Image(systemName: "pencil.tip.crop.circle.badge.minus")
-                            .font(.headline)
-                    })
+                            .font(.title)
+                    }
                     
-//                    Button(action: {
-//
-//                        isDraw = true
-//                        type = .pencil
-//
-//                    }, label: {
-//                        Image(systemName: "pencil")
-//                    })
+                    // Menu
                     
-//                    Button(action: {
-//
-//                        isDraw = true
-//                        type = .marker
-//
-//                    }, label: {
-//                        Image(systemName: "highlighter")
-//                    })
-//
-//                    if #available(iOS 14.0, *) {
-//                        ColorPicker(selection: $color) {
-//
-//                        }
-//                    } else {
-//                        // Fallback on earlier versions
-//                    }
+                    Menu {
+                        Button(action: {
+                            colorPicker.toggle()
+                        }) {
+                            Label {
+                                Text("Color")
+                            } icon: {
+                                Image(systemName: "eyedropper.halffull")
+                            }
+                        }
+                        
+                        Button(action: {
+                            isDraw = true
+                            type = .pencil
+                        }) {
+                            Label {
+                                Text("Pencil")
+                            } icon: {
+                                Image(systemName: "pencil")
+                            }
+                        }
+                        
+                        Button(action: {
+                            
+                            isDraw = true
+                            type = .pen
+                            
+                        }) {
+                            Label {
+                                Text("Pen")
+                            } icon: {
+                                Image(systemName: "pencil.tip")
+                            }
+                        }
+                        
+                        Button(action: {
+                            isDraw = true
+                            type = .marker
+                        }) {
+                            Label {
+                                Text("Marker")
+                            } icon: {
+                                Image(systemName: "highlighter")
+                            }
+                        }
+                        
+                    } label: {
+                        Image(systemName: "pencil")
+                            .resizable()
+//                            .frame(width: 25, height: 25)
+                    }
+                })
+                .sheet(isPresented: $colorPicker) {
+                    ColorPicker("Pick a Color", selection: $color)
                 }
-                
-                )
-        }
+        }.padding()
     }
     
     func SaveImage() {
         let image = canvas.drawing.image(from: canvas.drawing.bounds, scale: 1)
-        
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
 }
-
-struct DrawingView: UIViewRepresentable {
+struct DrawingView : UIViewRepresentable {
     
     @Binding var canvas : PKCanvasView
     @Binding var isDraw : Bool
-    @Binding var type: PKInkingTool.InkType
-    @Binding var color: Color
+    @Binding var type : PKInkingTool.InkType
+    @Binding var color : Color
     
-    let ink = PKInkingTool(.marker, color: .gray)
-    
-//    var ink: PKInkingTool {
-//
-//        PKInkingTool(type, color: UIColor(color))
-//
-//    }
+    var ink: PKInkingTool {
+        PKInkingTool(type, color: UIColor(color))
+    }
     
     let eraser = PKEraserTool(.bitmap)
     
     func makeUIView(context: Context) -> PKCanvasView {
-        
-        if #available(iOS 14.0, *) {
-            canvas.drawingPolicy = .anyInput
-            canvas.tool = isDraw ? ink : eraser
-            
-        } else {
-            // Fallback on earlier versions
-        }
+        canvas.drawingPolicy = .anyInput
+        canvas.tool = isDraw ? ink : eraser
         return canvas
     }
     
     func updateUIView(_ uiView: PKCanvasView, context: Context) {
-        //
-        
         uiView.tool = isDraw ? ink : eraser
     }
-    
-
 }
